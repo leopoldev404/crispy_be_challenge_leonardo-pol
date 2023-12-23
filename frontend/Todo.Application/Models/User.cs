25a7792313fe8965a123a2a36693aca9677a0f42
leonardo.pol <leonardo.pol@vodafone.com>
2023-12-23 02:19:02 +0100

    refactor(common): remove frontend | refactor

--- frontend/Todo.Application/Models/User.cs
+++
@@ -1,3 +0,0 @@
-namespace Todo.Application;
-
-public record User(string Username, string Password);
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,8 @@
 echo "Starting Setup..."
-export POSTGRES_USER=dev
+echo "Setting Environment Variables..."
+export POSTGRES_USERNAME=dev
 export POSTGRES_PASSWORD=devpsw
+export SERVICE_API_KEY=key
 docker-compose -f docker/docker-compose.yml up --build
 echo "Init Containers! 🐋"
 echo "Setup Completed! 🚀"
